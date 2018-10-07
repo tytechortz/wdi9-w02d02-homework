544581b74abbf833236bad11ddfdf502e6b23da5
James <tytechortz@gmail.com>
2018-10-06 23:38:11 -0600

    adds more scoring features

--- app.js
+++ app.js
@@ -61,14 +61,15 @@ let eggBert = {
    name: 'Eggbert',
    cardsDealt: [],
    cardsPlayed: 0,
-   score: 0,
+   roundsWon: 0,
+   
 };
 
 let computer = {
   name: 'Computer',
   cardsDealt: [],
   carsPlayed: 0,
-  score: 0,
+  roundsWon: 0,
 };
 
 
@@ -107,32 +108,99 @@ $("#start").click(function() {
 
 
 const battle1 = () => {
+  let eggBertScore = 0;
+  let computerScore = 0;
   for(let i = 0; i < 3; i++) {
     if(eggBert.cardsDealt[i].damage > computer.cardsDealt[i].damage) {
-      eggBert.score ++;
+      eggBertScore ++;
     } else if (
       computer.cardsDealt[i].damage > eggBert.cardsDealt[i].damage) {
-      computer.score ++;
+      computerScore ++;
+    } 
+  }
+  if(eggBertScore > computerScore) {
+    eggBert.roundsWon ++;
+    $('#eggBert-roundsWon').text(eggBert.roundsWon);
+  } else if
+  (computerScore > eggBertScore) {
+    computer.roundsWon ++;
+    $('#computer-roundsWon').text(computer.roundsWon);
+  };
+  console.log(eggBert.roundsWon);
+  console.log(computer.roundsWon);
+}
+
+
+const battle2 = () => {
+  let eggBertScore = 0;
+  let computerScore = 0;
+  for(let i = 3; i < 6; i++) {
+    if(eggBert.cardsDealt[i].damage > computer.cardsDealt[i].damage) {
+      eggBertScore ++;
+    } else if (
+      computer.cardsDealt[i].damage > eggBert.cardsDealt[i].damage) {
+      computerScore ++;
     }
   }
+  if(eggBertScore > computerScore) {
+    eggBert.roundsWon ++;
+    $('#eggBert-roundsWon').text(eggBert.roundsWon);
+  } else if
+  (computerScore > eggBertScore) {
+    computer.roundsWon ++;
+    $('#computer-roundsWon').text(computer.roundsWon);
+  }; 
+  console.log(eggBert.roundsWon);
+  console.log(computer.roundsWon);  
+};
+
+const battle3 = () => {
+  let eggBertScore = 0;
+  let computerScore = 0;
+  for(let i = 6; i < 9; i++) {
+  
+    if(eggBert.cardsDealt[i].damage > computer.cardsDealt[i].damage) {
+      eggBertScore ++;
+    } else if (
+      computer.cardsDealt[i].damage > eggBert.cardsDealt[i].damage) {
+      computerScore ++;
+    }
+  }
+  if(eggBertScore > computerScore) {
+    eggBert.roundsWon ++;
+    $('#eggBert-roundsWon').text(eggBert.roundsWon);
+  } else if
+  (computerScore > eggBertScore) {
+    computer.roundsWon ++;
+    $('#computer-roundsWon').text(computer.roundsWon);
+  }
+  console.log(eggBert.roundsWon);
+  console.log(computer.roundsWon);
 };
 
 
-$("#test").click(function() {
+$("#rnd1").click(function() {
   battle1();
   $("#battle").append(`<li>${eggBert.cardsDealt[0].name}-${eggBert.cardsDealt[0].damage} vs ${computer.cardsDealt[0].name} ${computer.cardsDealt[0].damage}</li>`);
   $("#battle").append(`<li>${eggBert.cardsDealt[1].name}-${eggBert.cardsDealt[1].damage} vs ${computer.cardsDealt[1].name} ${computer.cardsDealt[1].damage}</li>`);
   $("#battle").append(`<li>${eggBert.cardsDealt[2].name}-${eggBert.cardsDealt[2].damage} vs ${computer.cardsDealt[2].name} ${computer.cardsDealt[2].damage}</li>`);
-  console.log(eggBert.score);
-  console.log(computer.score);
+ 
+})
+
+$("#rnd2").click(function() {
+  battle2();
+  $("#battle").append(`<li>${eggBert.cardsDealt[3].name}-${eggBert.cardsDealt[3].damage} vs ${computer.cardsDealt[3].name} ${computer.cardsDealt[3].damage}</li>`);
+  $("#battle").append(`<li>${eggBert.cardsDealt[4].name}-${eggBert.cardsDealt[4].damage} vs ${computer.cardsDealt[4].name} ${computer.cardsDealt[4].damage}</li>`);
+  $("#battle").append(`<li>${eggBert.cardsDealt[5].name}-${eggBert.cardsDealt[5].damage} vs ${computer.cardsDealt[5].name} ${computer.cardsDealt[5].damage}</li>`);
+ 
+})
+
+$("#rnd3").click(function() {
+  battle3();
+  $("#battle").append(`<li>${eggBert.cardsDealt[6].name}-${eggBert.cardsDealt[6].damage} vs ${computer.cardsDealt[6].name} ${computer.cardsDealt[6].damage}</li>`);
+  $("#battle").append(`<li>${eggBert.cardsDealt[7].name}-${eggBert.cardsDealt[7].damage} vs ${computer.cardsDealt[7].name} ${computer.cardsDealt[7].damage}</li>`);
+  $("#battle").append(`<li>${eggBert.cardsDealt[8].name}-${eggBert.cardsDealt[8].damage} vs ${computer.cardsDealt[8].name} ${computer.cardsDealt[8].damage}</li>`);
+   
 })
 
 
-
-
-// // Divide first 6 cards to each player, 3 each
-// eggBert.cardsDealt = firstSixCards.slice(0, 3);
-// computer.cardsDealt = firstSixCards.slice(3, 6);
-
-// //game play
-// function play() 
